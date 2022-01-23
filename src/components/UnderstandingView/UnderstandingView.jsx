66ab36bdff662900226b6728a490093902e74238
Christopher Razidlo <cd.razidlo@gmail.com>
2022-01-23 14:19:13 -0600

    validation added to understanding view

--- src/components/UnderstandingView/UnderstandingView.jsx
+++ src/components/UnderstandingView/UnderstandingView.jsx
@@ -1,19 +1,21 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 
 function UnderstandingView() {
 
-    // Store access and dispatch hook
+    // Store access, dispatch hook, history hook
     const feedback = useSelector(store => store.feedbackReducer);
     const dispatch = useDispatch();
+    const history = useHistory();
 
     // State variable for understanding score
     const [understandingScore, setUnderstandingScore] = useState(feedback.understanding);
 
     // Declare onContinue function
-    const onContinue = () => {
+    const onContinue = event => {
+        event.preventDefault();
         console.log('in onContinue in UnderstandingView');
         console.log('Updating understanding score in store: ', understandingScore);
         // Dispatch understanding score to redux store
@@ -24,24 +26,27 @@ function UnderstandingView() {
                 understanding: Number(understandingScore)
             }
         });
+        // Navigate to the next page
+        history.push('/support');
     }
 
     return (
         <>
         <h3>How well are you understanding the material?</h3>
-        <input 
+        <form onSubmit={onContinue}>
+            <input 
+                required
                 id="understanding_score"
                 name="understanding"
                 type="number"
                 value={understandingScore}
                 onChange={event => setUnderstandingScore(event.target.value)}
             />
-            <br></br>
-        <Link to="/support">
-            <button onClick={onContinue}>
-                Continue
-            </button>
-        </Link>
+            <input
+                type="submit"
+                value="Continue"
+            />
+        </form>
         </>
     )
 }
