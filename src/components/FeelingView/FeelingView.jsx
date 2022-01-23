f2881fe1d83765ff8cda62d55e38d56d9a387aab
Christopher Razidlo <cd.razidlo@gmail.com>
2022-01-22 19:47:21 -0600

    setup redux store in index.js, added input to feedback

--- src/components/FeelingView/FeelingView.jsx
+++ src/components/FeelingView/FeelingView.jsx
@@ -3,8 +3,14 @@ import { Link } from 'react-router-dom';
 function FeelingView() {
     return (
         <>
-            <h3>Feeling</h3>
-
+            <h3>How are you feeling today?</h3>
+            <input 
+                id="feeling_score"
+                name="feeling"
+                type="number"
+            />
+            <label for="feeling_score">Feeling?</label>
+            <br></br>
             <Link to="/understanding">
                 <button>
                     Continue
