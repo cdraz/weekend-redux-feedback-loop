ea7141ae451b9740c145e6bd8cbc92fcfb15e346
Christopher Razidlo <cd.razidlo@gmail.com>
2022-01-23 14:23:55 -0600

    validation added to comments view

--- src/components/SupportView/SupportView.jsx
+++ src/components/SupportView/SupportView.jsx
@@ -1,18 +1,20 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 
 function SupportView() {
-    // Store access and dispatch hook
+    // Store access, dispatch hook, history hook
     const feedback = useSelector(store => store.feedbackReducer);
     const dispatch = useDispatch();
+    const history = useHistory();
 
     // State variable for support score
     const [supportScore, setSupportScore] = useState(feedback.support);
 
     // Declare onContinue function
-    const onContinue = () => {
+    const onContinue = event => {
+        event.preventDefault();
         console.log('in onContinue in SupportView');
         console.log('Updating support score in store: ', supportScore);
         // Dispatch support score to redux store
@@ -23,24 +25,27 @@ function SupportView() {
                 support: Number(supportScore)
             }
         });
+        // Navigate to the next page
+        history.push('/comments');
     }
 
     return (
         <>
             <h3>How well are you being supported?</h3>
-            <input 
-                id="support_score"
-                name="support"
-                type="number"
-                value={supportScore}
-                onChange={event => setSupportScore(event.target.value)}
-            />
-            <br></br>
-            <Link to="/comments">
-                <button onClick={onContinue}>
-                    Continue
-                </button>
-            </Link>
+            <form onSubmit={onContinue}>
+                <input
+                    required
+                    id="support_score"
+                    name="support"
+                    type="number"
+                    value={supportScore}
+                    onChange={event => setSupportScore(event.target.value)}
+                />
+                <input
+                    type="submit"
+                    value="Continue"
+                />
+            </form>
         </>
     )
 }
